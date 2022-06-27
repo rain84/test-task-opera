9ceab1f79e637d9294dcd58caaa8f1cac32cd8bc
rain84 <rainy_sky@mail.ru>
2022-06-27 11:11:04 +0300

    ✨ feat: added app routing

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -3,15 +3,13 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from 'layout'
 
 export const App = () => (
-  <div className="relative overflow-hidden bg-white rounded-3xl w-[var(--app-width)] h-[var(--app-height)]">
-    <Routes>
-      <Route path="/">
-        <Route index element={<DefaultRoute />} />
-        <Route path="home" element={<Home />} />
-        <Route path="*" element={<DefaultRoute />} />
-      </Route>
-    </Routes>
-  </div>
+  <Routes>
+    <Route path="/">
+      <Route index element={<DefaultRoute />} />
+      <Route path="home" element={<Home />} />
+      <Route path="*" element={<DefaultRoute />} />
+    </Route>
+  </Routes>
 )
 
 function DefaultRoute() {
