ef661ed9a8e5275ad4ff34538ae080c34b863cd6
rain84 <rainy_sky@mail.ru>
2022-06-27 11:10:29 +0300

    ✨ feat: added layout for the Home-view

--- src/layout/home.component.tsx
+++ src/layout/home.component.tsx
@@ -1,9 +1,15 @@
 export const Home = () => {
   return (
-    <section>
-      <h1 className="text-4xl font-extrabold tracking-tight text-center text-gray-900 sm:text-5xl md:text-6xl">
-        Home
-      </h1>
-    </section>
+    <div className="page">
+      <header className="h-[var(--app-header-height)] flex justify-between items-center p-[var(--app-padding)]">
+        <h1 className="text-app-title">Household</h1>
+      </header>
+      <main className="flex-1 border-2 border-red">
+        <p>Main</p>
+      </main>
+      <footer className="border-2 border-green-600">
+        <p>Footer</p>
+      </footer>
+    </div>
   )
 }
