4d28d46248c368a19fb1d044c9d480879d6e43ba
rain84 <rainy_sky@mail.ru>
2022-07-13 09:43:30 +0300

    ♻️ refact: updated props initialization refact: stories improves

--- src/ui/button/button-mode/button-mode.component.tsx
+++ src/ui/button/button-mode/button-mode.component.tsx
@@ -1,6 +1,6 @@
 import { Button, type ButtonProps } from '../button.component'
 
-export const ButtonMode = ({ className, ...props }: ButtonProps) => (
+export const ButtonMode = ({ className = '', ...props }: ButtonProps) => (
   <Button
     {...props}
     className={`rounded-2xl elevation-big w-[80px] h-[80px] border-solid hover:border-2 hover:border-primary ${className}`}
--- src/ui/button/button-usage/button-usage.component.tsx
+++ src/ui/button/button-usage/button-usage.component.tsx
@@ -1,6 +1,6 @@
 import { Button, type ButtonProps } from '../button.component'
 
-export const ButtonUsage = ({ className, ...props }: ButtonProps) => (
+export const ButtonUsage = ({ className = '', ...props }: ButtonProps) => (
   <Button
     {...props}
     className={`bg-white rounded-2xl w-[56px] h-[56px] border-solid hover:border-2 hover:border-primary ${className}`}
