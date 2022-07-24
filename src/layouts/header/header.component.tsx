f2d86f44706975ba11046d290ee59211ce410c30
rain84 <rainy_sky@mail.ru>
2022-07-24 15:48:11 +0300

    ✨ feat: added storybook for <Header/> 💄 refact: <Header/> UI updated

--- src/layouts/header/header.component.tsx
+++ src/layouts/header/header.component.tsx
@@ -1,13 +1,15 @@
 import { useCallback, useState } from 'react'
 import { Button, IconMenu, User } from 'ui'
 
-export const Header = () => {
+export const Header = ({ className }: ComponentProps) => {
   const [focus, setFocus] = useState(true)
   const onClick = useCallback(() => setFocus(false), [])
 
   return (
-    <header className="h-[var(--app-header-height)] flex justify-between items-center p-[var(--app-padding)]">
-      <Button>
+    <header
+      className={`h-[var(--app-header-height)] min-w-full flex justify-between items-center p-[var(--app-padding)] ${className}`.trimEnd()}
+    >
+      <Button className="mr-[22px]">
         <IconMenu className="icon-black hover:bg-selected" />
       </Button>
       <h1 className="text-left text-app-title grow">Household</h1>
