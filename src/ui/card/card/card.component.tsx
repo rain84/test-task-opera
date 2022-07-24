b7a758ff493c8dbf39ca82cb241103fa284fb402
rain84 <rainy_sky@mail.ru>
2022-07-24 15:26:11 +0300

    💄 refact: <Card/> UI updated

--- src/ui/card/card/card.component.tsx
+++ src/ui/card/card/card.component.tsx
@@ -12,7 +12,7 @@ export const Card = ({ className = '', title, label, children }: CardProps) => (
     className={`flex flex-col items-center justify-center ${className}`.trimEnd()}
   >
     {children}
-    {title && <p className="text-subtitle-bold">{title}</p>}
+    {title && <p className="mt-1 text-subtitle-1-bold">{title}</p>}
     {label && (
       <p className="text-body text-low-emphasis whitespace-nowrap">{label}</p>
     )}
